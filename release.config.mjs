cda6b8674e8807b8944bda17e8f6882a00c225ea
Subhashish Nabajja <subhashishnabajja1575643@gmail.com>
2024-10-16 22:25:21 +0530

    chore: Initial commit

---
+++ release.config.mjs
@@ -0,0 +1,29 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: [
+    "main",
+    {
+      name: "dev-release",
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    {
+      path: "@semantic-release/changelog",
+      changelogFile: "CHANGELOG.md",
+    },
+    {
+      path: "@semantic-release/npm",
+      npmPublish: false,
+    },
+    {
+      path: "@semantic-release/git",
+      assets: ["CHANGELOG.md", "package.json"],
+    },
+    "@semantic-release/github",
+  ],
+};
